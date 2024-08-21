2017c5c2dca886876ee887787752369e0bddd1b3
Oleksandr Patsuryn <opatsurin@gmail.com>
2024-08-21 14:25:20 +0300

    background size fix

--- script.js
+++ script.js
@@ -261,13 +261,6 @@ async function updateUser(updateData) {
 document.addEventListener('DOMContentLoaded', () => {
     showLoader();
 
-    const overflow = 100
-    document.body.style.overflowY = 'hidden'
-    document.body.style.marginTop = `${overflow}px`
-    document.body.style.height = window.innerHeight + overflow + "px"
-    document.body.style.paddingBottom = `${overflow}px`
-    window.scrollTo(0, overflow)
-
     let ts;
     const onTouchStart = (e) => {
         ts = e.touches[0].clientY;
